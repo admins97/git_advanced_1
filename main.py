111863961b985937197c50f33b56636aa4926df7
admin97 <alstjr88@g.skku.edu>
2023-03-30 19:28:09 +0900

    list square sum

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
         The sum of the squares of all even numbers in the list.
     """
     # TODO: Implement sum_of_squares_of_even
-    return sum(even_int_list)
+    return sum((even_int_list)**2)
 
 # Main function
 def main():
